5c750b243d5dc17173e323ba660eff0f93d68123
yaron-meister <67842516+yaron-meister@users.noreply.github.com>
2021-01-16 08:40:35 +0200

    Finishing Scheduler test

--- projects/wd_cross_platform/scheduler/scheduler_test.cpp
+++ projects/wd_cross_platform/scheduler/scheduler_test.cpp
@@ -1,20 +1,217 @@
-// scheduler.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
+/*****************************************************************************
+ * File name:   scheduler_test.c
+ * Developer:   Yaron Meister
+ * Date:        2018-11-26 17:35:25
+ * Description:
+ * Test Flow:
+ *****************************************************************************/
 
-#include <iostream>
+#include  <functional>
 
-int main()
+#include "scheduler.h"
+#include "tests.h"
+
+/* Forward Declarations    */
+
+CScheduler::CTask::ETaskRepetition printExampleFunc1(ParamsBase* pParams);
+CScheduler::CTask::ETaskRepetition printExampleFunc2(ParamsBase* pParams);
+CScheduler::CTask::ETaskRepetition printExampleFunc3(ParamsBase* pParams);
+CScheduler::CTask::ETaskRepetition printExampleFunc4(ParamsBase* pParams);
+CScheduler::CTask::ETaskRepetition stopFunc(ParamsBase* pParams);
+
+class SchedulerTest : public Test
 {
-    std::cout << "Hello World!\n";
+public:
+  using TestActionFunc = std::function<CScheduler::CTask::ETaskRepetition(ParamsBase* pParams)>;
+
+  void addTaskTest();
+  void removeTaskTest();
+  void runStopTest();
+  void taskCountTest();
+  void isEmptyTest();
+
+  static void incrementFunc1Runs() { ++m_func1RunsNum; }
+  static void incrementFunc2Runs() { ++m_func2RunsNum; }
+  static unsigned short getFunc1Runs() { return (m_func1RunsNum); }
+  static unsigned short getFunc2Runs() { return (m_func2RunsNum); }
+
+private:
+  static unsigned short m_func1RunsNum;
+  static unsigned short m_func2RunsNum;
+};
+
+/*  Global/Static variables  */
+unsigned short SchedulerTest::m_func1RunsNum = 0;
+unsigned short SchedulerTest::m_func2RunsNum = 0;
+
+
+/*******************************************************************************
+                                Main Function
+*******************************************************************************/
+int main(void)
+{
+  SchedulerTest schedulerTest;
+
+  schedulerTest.addTaskTest();
+  schedulerTest.removeTaskTest();
+  schedulerTest.runStopTest();
+  schedulerTest.taskCountTest();
+  schedulerTest.isEmptyTest();
+
+  return (EXIT_SUCCESS);
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
+/******************************************************************************/
+void SchedulerTest::addTaskTest()
+{
+  CScheduler scheduler;
 
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
+  printf("\n******************* Add - TEST ******************************\n");
+
+  time_t intervalS1(0);
+  CUid uid = scheduler.addTask(TestActionFunc(printExampleFunc1), nullptr, intervalS1);
+  equalityTest<bool>(uid.isBad(), true);
+
+  time_t intervalS2(5);
+  uid = scheduler.addTask(TestActionFunc(printExampleFunc2), nullptr, intervalS2);
+  equalityTest<size_t>(uid.getCounter(), 3);
+
+  
+  time_t intervalS3(12);
+  scheduler.addTask(TestActionFunc(printExampleFunc3), nullptr, intervalS3);
+  equalityTest<size_t>(scheduler.taskCount(), 2);
+}
+
+/******************************************************************************/
+void SchedulerTest::removeTaskTest()
+{
+  CScheduler scheduler;
+  EStatus removeStatus(SUCCESS);
+  time_t intervalS2(5);
+
+  printf("\n******************* Remove - TEST ***************************\n");
+
+  CUid uid;
+  removeStatus = scheduler.removeTask(uid);
+  equalityTest<EStatus>(removeStatus, FAILURE);
+  
+  time_t intervalS1(3);
+  scheduler.addTask(TestActionFunc(printExampleFunc1), nullptr, intervalS1);
+  removeStatus = scheduler.removeTask(uid);
+  equalityTest<EStatus>(removeStatus, FAILURE);
+
+  uid = scheduler.addTask(TestActionFunc(printExampleFunc2), nullptr, intervalS2);
+  removeStatus = scheduler.removeTask(uid);
+  equalityTest<EStatus>(removeStatus, SUCCESS);
+  equalityTest<size_t>(scheduler.taskCount(), 1);
+}
+
+/******************************************************************************/
+void SchedulerTest::runStopTest()
+{
+  CScheduler scheduler;
+
+  printf("\n********************* Run/Stop - TEST ***********************\n");
+
+  time_t intervalS1(4);
+  time_t intervalS2(3);
+  time_t intervalS3(10);
+  scheduler.addTask(TestActionFunc(printExampleFunc1), nullptr, intervalS1);
+  scheduler.addTask(TestActionFunc(printExampleFunc2), nullptr, intervalS2);
+  scheduler.addTask(TestActionFunc(stopFunc), (ParamsBase*)&scheduler, intervalS3);
+  scheduler.run();
+  equalityTest<unsigned short>(SchedulerTest::getFunc1Runs(), 2);
+  equalityTest<unsigned short>(SchedulerTest::getFunc2Runs(), 3);
+}
+
+/******************************************************************************/
+void SchedulerTest::taskCountTest()
+{
+  CScheduler scheduler;
+
+  printf("\n******************* Count - TEST ****************************\n");
+
+  equalityTest<size_t>(scheduler.taskCount(), 0);
+  
+  time_t intervalS1(2);
+  time_t intervalS2(5);
+  time_t intervalS3(12);
+  scheduler.addTask(TestActionFunc(printExampleFunc1), nullptr, intervalS1);
+  scheduler.addTask(TestActionFunc(printExampleFunc2), nullptr, intervalS2);
+  scheduler.addTask(TestActionFunc(printExampleFunc3), nullptr, intervalS3);
+  equalityTest<size_t>(scheduler.taskCount(), 3);
+}
+
+/******************************************************************************/
+void SchedulerTest::isEmptyTest()
+{
+  CScheduler scheduler;
+
+  printf("\n******************* IsEmpty - TEST **************************\n");
+
+  equalityTest<bool>(scheduler.isEmpty(), true);
+  
+  time_t intervalS1(2);
+  time_t intervalS2(5);
+  time_t intervalS3(12);
+  CUid uid1 = scheduler.addTask(TestActionFunc(printExampleFunc1), nullptr, intervalS1);
+  CUid uid2 = scheduler.addTask(TestActionFunc(printExampleFunc2), nullptr, intervalS2);
+  CUid uid3 = scheduler.addTask(TestActionFunc(printExampleFunc3), nullptr, intervalS3);
+  equalityTest<bool>(scheduler.isEmpty(), false);
+
+  scheduler.removeTask(uid1);
+  scheduler.removeTask(uid2);
+  scheduler.removeTask(uid3);
+  equalityTest<bool>(scheduler.isEmpty(), true);
+}
+
+/******************************************************************************/
+CScheduler::CTask::ETaskRepetition printExampleFunc1(ParamsBase* pParams)
+{
+  UNUSED_VAR(pParams);
+  cout << "\n\n***Function 1 is working***\n\n";
+  SchedulerTest::incrementFunc1Runs();
+
+  return (CScheduler::CTask::TASK_REPEAT);
+}
+
+/******************************************************************************/
+CScheduler::CTask::ETaskRepetition printExampleFunc2(ParamsBase* pParams)
+{
+  UNUSED_VAR(pParams);
+  cout << "\n\n***Function 2 is working***\n\n";
+  SchedulerTest::incrementFunc2Runs();
+
+  return (CScheduler::CTask::TASK_REPEAT);
+}
+
+/******************************************************************************/
+CScheduler::CTask::ETaskRepetition printExampleFunc3(ParamsBase* pParams)
+{
+  UNUSED_VAR(pParams);
+  cout << "\n\n***Function 3 is working***\n\n";
+
+  return (CScheduler::CTask::TASK_ERROR);
+}
+
+/******************************************************************************/
+CScheduler::CTask::ETaskRepetition printExampleFunc4(ParamsBase* pParams)
+{
+  UNUSED_VAR(pParams);
+  cout << "\n\n***Function 3 is working***\n\n";
+
+  return (CScheduler::CTask::TASK_ERROR);
+}
+
+/******************************************************************************/
+CScheduler::CTask::ETaskRepetition stopFunc(ParamsBase* pParams)
+{
+  cout << "\n\n***StopFunc is working***\n\n";
+
+  if (pParams)
+  {
+    ((CScheduler*)pParams)->stop();
+  }
+
+  return (CScheduler::CTask::TASK_NOREPEAT);
+}
