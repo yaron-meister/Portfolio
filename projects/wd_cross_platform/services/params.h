0954602621e0bc30a56567e59df6a0206cbb116a
yaron-meister <67842516+yaron-meister@users.noreply.github.com>
2021-02-06 23:01:12 +0200

    Updating services

--- projects/wd_cross_platform/services/params.h
+++ projects/wd_cross_platform/services/params.h
@@ -4,5 +4,10 @@
  * Description: Base class for passing Params by reference
  ******************************************************************************/
 
+#ifndef __PARAMS_BASE_H__
+#define __PARAMS_BASE_H__
+
 class ParamsBase
 {};
+
+#endif //__PARAMS_BASE_H__
