93bc84039f5481f4f1418f11f2b1d1f96cc06942
yaronme <yaron@skysoftsl.com>
2021-05-28 10:52:07 +0300

    Params Observable

--- courses/angular/routing-demo/src/app/department-detail/department-detail.component.ts
+++ courses/angular/routing-demo/src/app/department-detail/department-detail.component.ts
@@ -1,20 +1,40 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Params, Router } from '@angular/router';
 
 @Component({
   selector: 'app-department-detail',
   template: `
-    <p>
-      department-detail works!
-    </p>
+    <h3> Yoy selected department with id = {{departmentId}} </h3>
+    <button (click)="goPrevious()"> Previous </button>
+    <button (click)="goNext()"> Next </button>
   `,
   styles: [
   ]
 })
 export class DepartmentDetailComponent implements OnInit {
 
-  constructor() { }
+  public departmentId;
+
+  constructor(private route: ActivatedRoute, private router: Router) { }
 
   ngOnInit(): void {
+    // The Snapshot approach - Causing a Drawback
+    // let id = parseInt(this.route.snapshot.paramMap.get('id'));
+    // this.departmentId = id;
+
+    this.route.paramMap.subscribe((params: Params) => {
+      let id = parseInt(params.get('id'));
+      this.departmentId = id;
+    });
   }
 
+  goPrevious(){
+    let previousId = this.departmentId - 1;
+    this.router.navigate(['/departments', previousId]);
+  }
+  
+  goNext(){
+    let nextId = this.departmentId + 1;
+    this.router.navigate(['/departments', nextId]);
+  }
 }
