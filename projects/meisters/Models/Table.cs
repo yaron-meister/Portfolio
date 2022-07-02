5dfc7d3ac35980738ae9080f13ad7c4514d74dc1
yaron-meister <yaronmeister1@gmail.com>
2022-07-02 18:43:55 +0300

    Adding the Table's Status colors

---
+++ projects/meisters/Models/Table.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Meisters.Models
+{
+    public enum ETableStatus
+    {
+        Clear,
+        NoOrder,
+        Order,
+        Check
+    }
+
+    class Table
+    {
+    }
+}
---
+++ projects/meisters/Services/Controls.cs
@@ -0,0 +1,24 @@
+﻿using Meisters.Models;
+using Meisters.Views;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace Meisters.Services
+{
+    public class TableCheckBox : CheckBox
+    {
+        public ETableStatus Status
+        {
+            get => (ETableStatus)GetValue(StatusProperty);
+            set => SetValue(StatusProperty, value);
+        }
+        public static readonly DependencyProperty StatusProperty =
+            DependencyProperty.Register(nameof(Status), typeof(ETableStatus),
+                                        typeof(TableCheckBox), new PropertyMetadata(ETableStatus.Clear));
+    }
+}
