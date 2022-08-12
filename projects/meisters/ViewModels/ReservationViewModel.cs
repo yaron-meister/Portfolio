f2769d7772b761b4bdad23894e633b1ab73464b8
yaron-meister <yaronmeister1@gmail.com>
2022-08-12 09:36:14 +0300

    Creating the Reservation view and its Left Top and Bottom toolbars

---
+++ projects/meisters/ViewModels/ReservationViewModel.cs
@@ -0,0 +1,38 @@
+﻿using GalaSoft.MvvmLight.Command;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace Meisters.ViewModels
+{
+    class ReservationViewModel : ViewModelBase
+    {
+
+        #region Commands
+
+        public ICommand PrintCheckCommand => new RelayCommand(() =>
+        {
+            
+        });
+
+        public ICommand PayCommand => new RelayCommand(() =>
+        {
+
+        });
+
+        public ICommand ExitCommand => new RelayCommand(() =>
+        {
+
+        });
+
+        public ICommand SendCommand => new RelayCommand(() =>
+        {
+
+        });
+
+        #endregion Commands
+    }
+}
